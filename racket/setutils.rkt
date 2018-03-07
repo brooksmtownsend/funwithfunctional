c1404e64520873747418e7fe7cf4d2f9ec2d4b99
Brooks Townsend <brooksmtownsend@gmail.com>
2018-03-07 09:27:06 -0500

    removed temp file

--- racket/setutils.rkt
+++ racket/setutils.rkt
@@ -4,6 +4,14 @@
   (lambda (l i)
     (cond
       ((null? l) #f)
-      (eq? (car l) i #t)
-      (includes? cdr (l) i)
+      ((eq? (car l) i) #t)
+      ((includes? (cdr l) i))
+      (else #f))))
+
+(define isSet?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((includes? (cdr l) (car l)) #f)
+      ((isSet? (cdr l)))
       (else #f))))
